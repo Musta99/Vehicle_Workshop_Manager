d0d7325f189a6f2146be3dacde72bc9085572b17
Musta99 <mustafizurrahman1403099@gmail.com>
2024-10-23 19:52:56 +0600

    Firebase Setup with Firebase Authentication, Cloud Firestore , Admin-Mechanic Functionalities

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  Color commonColor = Color(0xff009688);
+  Color backgroundColor = Color(0xffF5F5F5);
+  Color shadowColor = Colors.black.withOpacity(0.4);
+}
